f2ae460ee050b984995faababde7b72b838611cf
YHDF <unes.houdaifa@gmail.com>
2022-10-21 15:41:39 +0200

    Just throw me into the ocean

---
+++ src/WeatherApp.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './WeatherApp.css';
+import React from 'react';
+
+
+class WeatherApp extends React.Component{
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>Helllooooo</div>
+    )
+  }
+}
+
+  
+
+export default WeatherApp;
