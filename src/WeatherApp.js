060641d469c119dc4f2227c820d529546a5cccb5
mustakhl <musta.khalouk@gmail.com>
2022-10-21 16:57:46 +0200

    [WIP] working on API calls

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -4,6 +4,10 @@ import React from 'react';
 
 
 class WeatherApp extends React.Component{
+  api_key = "3d74c79ef4464434bb1f6e3602dd4a21"; // 1000 call per day free
+  longitude = 0;
+  latitude = 0;
+  limit = 5;
 
   constructor(props) {
     super(props);
@@ -11,8 +15,17 @@ class WeatherApp extends React.Component{
 
   }
 
-  componentDidMount() {
+  async getLatLonFromCity(city) {
+    const res = await fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit="+limit+"&appid="+api_key).json();
+    return res;
+  }
 
+  currentWeather(lon, lat) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+api_key)
+    .then((response) => response.json())
+  }
+
+  componentDidMount() {
   }
 
   render() {
