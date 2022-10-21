9dfa095c73a26571aa3e862750db165a4e7cd4fd
YHDF <unes.houdaifa@gmail.com>
2022-10-21 17:12:48 +0200

    fix

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,24 +3,30 @@ import React from 'react';
 
 
 class WeatherApp extends React.Component {
-  api_key = "3d74c79ef4464434bb1f6e3602dd4a21"; // 1000 call per day free
-  longitude = 0;
-  latitude = 0;
-  limit = 5;
+
 
   constructor(props) {
     super(props);
-
-
+    this.state = {
+      api_key : "3d74c79ef4464434bb1f6e3602dd4a21",// 1000 call per day free
+      longitude : 0,
+      latitude : 0,
+      limit : 5
+    }
+    this.getLatLonFromCity = this.getLatLonFromCity.bind(this);
+    this.currentWeather = this.currentWeather.bind(this);
+    /*this.setState(() => {
+      limit : 5
+    })*/
   }
 
   async getLatLonFromCity(city) {
-    const res = await fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit="+limit+"&appid="+api_key).json();
+    const res = await fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit="+this.state.limit+"&appid="+this.state.api_key).json();
     return res;
   }
 
   currentWeather(lon, lat) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+api_key)
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+this.state.api_key)
     .then((response) => response.json())
   }
 
