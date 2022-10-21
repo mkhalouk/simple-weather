5d00407fe84a2af8cb2cce73210752ef091c5717
mustakhl <musta.khalouk@gmail.com>
2022-10-21 17:58:23 +0200

    Jeez this API thingy

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -15,19 +15,29 @@ class WeatherApp extends React.Component {
     }
     this.getLatLonFromCity = this.getLatLonFromCity.bind(this);
     this.currentWeather = this.currentWeather.bind(this);
-    /*this.setState(() => {
-      limit : 5
-    })*/
   }
 
   async getLatLonFromCity(city) {
-    const res = await fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit="+this.state.limit+"&appid="+this.state.api_key).json();
-    return res;
+    return await fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+this.state.api_key)
+    .then((response) => response.json());
   }
 
-  currentWeather(lon, lat) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+this.state.api_key)
-    .then((response) => response.json())
+  async currentWeather() {
+    const city = document.getElementById('city').value;
+    const coord = await this.getLatLonFromCity(city);
+
+    const data = await fetch("https://api.openweathermap.org/data/2.5/weather?lat="+coord[0].lat+"&lon="+coord[0].lon+"&appid="+this.state.api_key)
+    .then((response) => response.json());
+
+    /**
+     * @param object data
+     * in order to access to the weather infos uneed to typically use data.main
+     * (which contains temperature, pressure, humidity etc..). Uncomment the
+     * the following line to show all details
+     */
+    // console.log(data);
+
+    return data;
   }
 
   componentDidMount() {
@@ -40,7 +50,7 @@ class WeatherApp extends React.Component {
           <h2 className='title'>Weather forecast</h2>
           <div className='search-box'>
             <input class="cityname" id='city' type="text" name="cityname" placeholder='Entrer le nom de votre ville'></input>
-            <button className='submit' onClick={null}>Submit</button>
+            <button className='submit' onClick={this.currentWeather}>Submit</button>
           </div>
         </div>
       </div>
