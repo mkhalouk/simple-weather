5d1abc288ffdb1741569df7048e2b46045f6c461
YHDF <unes.houdaifa@gmail.com>
2022-10-21 16:59:37 +0200

    temporary

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './WeatherApp.css';
 import React from 'react';
 
 
-class WeatherApp extends React.Component{
+class WeatherApp extends React.Component {
 
   constructor(props) {
     super(props);
@@ -17,11 +16,19 @@ class WeatherApp extends React.Component{
 
   render() {
     return (
-      <div>Helllooooo</div>
+      <div className='bg'>
+        <div className='search-container'>
+          <h2 className='title'>Weather forecast</h2>
+          <div className='search-box'>
+            <input class="cityname" id='city' type="text" name="cityname" placeholder='Entrer le nom de votre ville'></input>
+            <button className='submit' onClick={null}>Submit</button>
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
-  
+
 
 export default WeatherApp;
